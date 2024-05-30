71c608363dc7739aace91f90646df448aeb55af8
RwxMurphy <amurfie2003@gmail.com>
2024-05-30 03:31:06 -0400

    add remove_bill method to User class

--- user.py
+++ user.py
@@ -12,22 +12,35 @@ class User:
         description: str = f"User name: {self.name} \nID:{self.user_id} \nUser Bills: {self.expenses}"
         return description
     
-    def add_new_bill(self, name: str, amount:float, due_date):
+    def add_new_bill(self, name, amount:float, due_date) -> None:
         name = Bill(name, amount, due_date)
         if type(name == Bill):
             if name not in self.expenses:
                 self.expenses.append(name)
 
+    def remove_bill(self, name: str) -> None:
+        for bill in self.expenses:
+            if bill.name == name:
+                self.expenses.remove(bill)
 
+    
+
+# Test the use and bill modules
 if __name__ == '__main__':
     #test user 1
     murphy =  User('Murphy', 123, 100)
     murphy.add_new_bill('phone', 90.50, datetime.date(2024,6,30))
     murphy.add_new_bill('cable', 60, datetime.date(2024,6,25))
     print(murphy)
+    
+    print()
 
     #test user 2
     mary =  User('Mary', 321, 100)
-    mary.add_new_bill('Hydro', 90.50, datetime.date(2024,6,30))
+    mary.add_new_bill('hydro', 90.50, datetime.date(2024,6,30))
     mary.add_new_bill('cable', 60, datetime.date(2024,6,25))
+
+    #remove a bill
+    mary.remove_bill('cable')
     print(mary)
+    
