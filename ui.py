665bc56ec2346601e02fadb86437d56939969668
RwxMurphy <amurfie2003@gmail.com>
2024-05-31 05:16:48 -0400

    add logo and sign in to welcome screen

--- ui.py
+++ ui.py
@@ -1,16 +1,19 @@
-welcome_msg = 'Welcome To Bills'
-welcome_screen:str = f'''
-{welcome_msg.center(120)} 
-_________________ |
-_________________ |
-_________________ |
-_________________ |
-_________________ |
-_________________ |
-_________________ |
-_________________ |
-_________________ |
+import shutil
+from bills_logo import bills_logo
 
-'''
+# get the size of the terminal window
+term_col_size = shutil.get_terminal_size().columns
+logo = bills_logo
+
+user_name: str = ''
+user_id: int = 0
+
+# Welcome screen
+print(logo) 
+print(f'--------------- Sign In ---------------\n' )
+user_name = input('Enter your user name: ')
+user_id = int(input('Enter your user id: '))
+
+
 if __name__ == '__main__':
-    print(welcome_screen)
+    print(f'\nuser name: {user_name} \nuser id: {user_id}')
