866e24cfd4985a572bf3a72e4859ee2c522d7a6e
RwxMurphy <amurfie2003@gmail.com>
2024-05-31 11:14:35 -0400

    build welcome screen and starting user selection menu

---
+++ main.py
@@ -0,0 +1,11 @@
+import ui
+
+ui.welcome_screen()
+ui.select_option()
+
+
+
+
+
+if __name__ == '__main__':
+    pass
--- ui.py
+++ ui.py
@@ -1,19 +1,42 @@
+import os
 import shutil
 from bills_logo import bills_logo
 
-# get the size of the terminal window
-term_col_size = shutil.get_terminal_size().columns
-logo = bills_logo
+def welcome_screen():
+    # get the size of the terminal window
+    term_col_size = shutil.get_terminal_size().columns
+    logo = bills_logo
 
-user_name: str = ''
-user_id: int = 0
+    # Welcome screen
+    print(logo) 
+    print(f'--------------- Sign In ---------------\n' )
+    
+# gets the users 
+def get_user_sign_in():
+    user_name: str = ''
+    user_id: int = 0
 
-# Welcome screen
-print(logo) 
-print(f'--------------- Sign In ---------------\n' )
-user_name = input('Enter your user name: ')
-user_id = int(input('Enter your user id: '))
+    user_name = input('Enter your user name: ')
+    user_id = int(input('Enter your user id: '))
+    return user_name, user_id
 
+
+# Menu selections
+def select_option() -> int:
+    user_name, user_id = get_user_sign_in() 
+    user_option_selected: int = 0
+    
+    menu_options = f"""
+--- Welcome {user_name} - {user_id} ---
+please enter the number that corresponds with the action you would like to take.
+1. View all you current bills.
+2. Add a new bill.
+3. Remove a bill
+"""
+    os.system('clear')
+    print(menu_options)
+    user_option_selected = input('What would you like to do: ')
+    return user_option_selected
 
 if __name__ == '__main__':
-    print(f'\nuser name: {user_name} \nuser id: {user_id}')
+    pass
